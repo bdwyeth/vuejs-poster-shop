b178001a070ac7ad2ca2c001315f51ddeacfdf3c
Ben Wyeth <bdwyeth@gmail.com>
2017-06-09 05:42:28 +1000

    Cart Functionality added

--- public/script.js
+++ public/script.js
@@ -1,3 +1,33 @@
 new Vue({
-	el: '#app'
+	el: '#app',
+	data: {
+		total: 0,
+		items:[
+			{title: 'Jenga', price: 10},
+			{title: 'Monopoly', price: 15},
+			{title: 'UNO', price: 5},
+		],
+		cart:[
+
+		]
+	},
+	methods: {
+		addItem: function(addedItem){
+			//check if already in cart
+			var itemIndex = _.findIndex(this.cart, function(o){return addedItem.title == o.title })
+			//doesnt exist, create qty key
+			if(itemIndex == -1){
+				addedItem.qty = 1;
+				this.cart.push(addedItem);
+			} else {
+				// Add to total property
+				this.cart[itemIndex].qty = ++this.cart[itemIndex].qty;
+			};
+
+			//Update the total
+			this.total = _.reduce(this.cart, function(subTotal, cartItem){
+				return subTotal + (cartItem.qty * cartItem.price);
+			}, 0)
+		}
+	}
 });
